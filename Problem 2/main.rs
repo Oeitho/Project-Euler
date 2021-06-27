545bcb47891d569711650196237fa00362b5a0d9
Øivind Thorrud <22742089+Oeitho@users.noreply.github.com>
2021-06-27 14:28:22 +0200

    Improve Problem 2 solution.

--- Problem 2/main.rs
+++ Problem 2/main.rs
@@ -1,18 +1,15 @@
 fn main() {
-	let mut fibonacci_values = vec![0, 1];
-	let mut i = 2;
+	let mut sum = 0;
+	let mut second_to_last = 0;
+	let mut last = 1;
 	loop {
-		let n = fibonacci_values[i - 2] + fibonacci_values[i - 1];
-		if n > 4_000_000 {
+		last = second_to_last + last;
+		second_to_last = last - second_to_last;
+		if last > 4_000_000 {
 			break;
 		}
-		fibonacci_values.push(n);
-		i += 1;
-	}
-	let mut sum = 0;
-	for value in fibonacci_values {
-		if value % 2 == 0 {
-			sum += value;
+		if last % 2 == 0 {
+			sum += last;
 		}
 	}
 	println!("{}", sum);
